44f2e2a879e10e6acf6b27ee7b8e360695ce1a0e
Virvon <dan130205@gmail.com>
2022-10-02 15:59:57 +0300

    local maximum

---
+++ local maximum/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace local_maximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrayLenght = 30;
+            int[] array = new int[arrayLenght];
+            Random random = new Random();
+            int minValue = 0;
+            int maxValue = 100;
+            int compareNumber;
+
+            Console.Write("Массив: ");
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(minValue, maxValue);
+                Console.Write(array[i] + " ");
+            }
+
+            Console.Write("\nЛокальные максимумы: ");
+
+            if (array[0] > array[1])
+            {
+                Console.Write(array[0] + " ");
+            }
+
+            for(int i = 1; i < array.Length - 2; i++)
+            {
+                if (array[i] > array[i + 1] && array[i] > array[i - 1])
+                {
+                    Console.Write(array[i] + " ");
+                }
+            }
+
+            if (array[array.Length - 1] > array[array.Length - 2])
+            {
+                Console.Write(array[array.Length - 1]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ local maximum/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("local maximum")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("local maximum")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("6bae7eac-e0c8-48e2-823e-4b2466e44f52")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
